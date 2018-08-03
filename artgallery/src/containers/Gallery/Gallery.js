df1a92dfcfdbfbb89789ab7ac024b5d5ed1d86d9
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-02 19:12:37 -0500

    the component for the gallery paintings and info

---
+++ artgallery/src/containers/Gallery/Gallery.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import Auxillary from '../../higherOrderComponents/Auxillary';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <Auxillary>
+                <div>paintings</div>
+                <div>info</div>
+            </Auxillary>
+        );
+    }
+}
+
+export default LandingPage;
