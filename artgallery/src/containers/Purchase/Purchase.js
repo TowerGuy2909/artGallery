d6800ff782b921452a2fd9d132ad81fca40ec6e2
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-02 19:13:40 -0500

    the container for the page to make and order

---
+++ artgallery/src/containers/Purchase/Purchase.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Auxillary from '../../higherOrderComponents/Auxillary';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <Auxillary>
+                <div>cost</div>
+                <div>order</div>
+                <div>delivery</div>
+            </Auxillary>
+        );
+    }
+}
+
+export default LandingPage;
