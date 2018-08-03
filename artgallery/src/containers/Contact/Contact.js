2af2f45170a25b0e7bf96734152925c2b9376e5b
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-02 19:11:53 -0500

    the component holding the contacts links and visitor comments

---
+++ artgallery/src/containers/Contact/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import Auxillary from '../../higherOrderComponents/Auxillary';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <Auxillary>
+                <div>links</div>
+                <div>comments</div>
+                <div>images</div>
+            </Auxillary>
+        );
+    }
+}
+
+export default LandingPage;
