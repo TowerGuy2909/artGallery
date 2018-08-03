a310503080d030cba1399ab43d3dedbd974e26b5
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-03 01:44:42 -0500

    fixed naming error

--- artgallery/src/containers/Purchase/Purchase.js
+++ artgallery/src/containers/Purchase/Purchase.js
@@ -2,16 +2,18 @@ import React, {Component} from 'react';
 
 import Auxillary from '../../higherOrderComponents/Auxillary';
 
-class LandingPage extends Component {
+class Purchase extends Component {
     render() {
         return (
             <Auxillary>
+                <div>
                 <div>cost</div>
                 <div>order</div>
                 <div>delivery</div>
+                </div>
             </Auxillary>
         );
     }
 }
 
-export default LandingPage;
+export default Purchase;
