942888fb0d45fb39313b279ba63318423088d16b
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-03 01:45:14 -0500

    changed place holders into links, imported link

--- artgallery/src/containers/Toolbar/Toolbar.js
+++ artgallery/src/containers/Toolbar/Toolbar.js
@@ -1,19 +1,21 @@
 import React, {Component} from 'react';
-
 import Auxillary from '../../higherOrderComponents/Auxillary';
+import { Link } from 'react-router-dom';
 
-class LandingPage extends Component {
+class Toolbar extends Component {
     render() {
         return (
             <Auxillary>
-                <div>LandingPage</div>
-                <div>About</div>
-                <div>Gallery</div>
-                <div>Contact</div>
-                <div>Purchase</div>
+                <ul>
+                    <li><Link to={'/'}>landingPage</Link></li>
+                    <li><Link to={'/about'}>About</Link></li>
+                    <li><Link to={'/gallery'}>Gallery</Link></li>
+                    <li><Link to={'/contact'}>Contact</Link></li>
+                    <li><Link to={'/purchase'}>Purchase</Link></li>
+                </ul>
             </Auxillary>
         );
     }
 }
 
-export default LandingPage;
+export default Toolbar;
