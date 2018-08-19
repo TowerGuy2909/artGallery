a93acdb35c22da018af54d4fb0afa26a51fc2c5f
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-18 23:19:06 -0500

    styled toolbar

--- artgallery/src/containers/Toolbar/Toolbar.js
+++ artgallery/src/containers/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Auxillary from '../../higherOrderComponents/Auxillary';
 import { Link } from 'react-router-dom';
+import classes from './Toolbar.css';
 
 class Toolbar extends Component {
     render() {
         return (
             <Auxillary>
-                <ul>
-                    <li><Link to={'/'}>landingPage</Link></li>
+                <ul className={classes.Toolbar}>
+                    <li><Link to={'/'}>Home</Link></li>
                     <li><Link to={'/about'}>About</Link></li>
                     <li><Link to={'/gallery'}>Gallery</Link></li>
                     <li><Link to={'/contact'}>Contact</Link></li>
