3078b195890ebda71e056f1fc3d5fa06fd27d43d
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-21 23:39:21 -0500

    collection of single links named dynamicly

---
+++ artgallery/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import classes from './NavigationItems.css';
+import SingleNavItem from './SingleNavItem/SingleNavItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <div className={classes.Logo}>Midnight Musing</div>
+        <SingleNavItem link='/'>Home</SingleNavItem>
+        <SingleNavItem link='/about'>About</SingleNavItem>
+        <SingleNavItem link='/gallery'>Gallery</SingleNavItem>
+        <SingleNavItem link='/contact'>Contact</SingleNavItem>
+        <SingleNavItem link='/purchase'>Purchase</SingleNavItem>
+    </ul>
+);
+
+export default navigationItems;
